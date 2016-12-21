ab6d91a9c4e5df542faf0fc45dbae8b97bbb82d7
Simon Larsson <snigelson@gmail.com>
2016-12-21 14:40:40 +0100

    Change tabs to spaces

--- tmcl.py
+++ tmcl.py
@@ -37,37 +37,37 @@ RFS_STATUS=2
 
 # Status code to string mapping
 status_string = {
-	100: "Successfully executed, no error",
-	101: "Command loaded into TMCL program EEPROM",
-	1:   "Wrong checksum",
-	2:   "Invalid command",
-	3:   "Wrong type",
-	4:   "Invalid value",
-	5:   "Configuration EEPROM locked",
-	6:   "Command not available"
+    100: "Successfully executed, no error",
+    101: "Command loaded into TMCL program EEPROM",
+    1:   "Wrong checksum",
+    2:   "Invalid command",
+    3:   "Wrong type",
+    4:   "Invalid value",
+    5:   "Configuration EEPROM locked",
+    6:   "Command not available"
 }
 
 def _calculate_checksum(data):
-	""" Calculate checksum.
-	Input:  Bytes object, string or array
-	Output: Integer
-	"""
-	return (sum(ord(b) for b in data))%256
+    """ Calculate checksum.
+    Input:  Bytes object, string or array
+    Output: Integer
+    """
+    return (sum(ord(b) for b in data))%256
 
 # handle is an object with read() and write() methods.
 
 def send_command(handle, address, command, type, motor, value):
-	data=struct.pack('>BBBBiB', address, command, type, motor, value)
-	data+=struct.pack('>B',_calculate_checksum(data))
-	handle.write(data)
+    data=struct.pack('>BBBBiB', address, command, type, motor, value)
+    data+=struct.pack('>B',_calculate_checksum(data))
+    handle.write(data)
 
 def get_result(handle, address, status, value):
-	data=handle.read()
-	
-	address,status,value,checksum=struct.unpack('>BxBxiB', data)
+    data=handle.read()
+    
+    address,status,value,checksum=struct.unpack('>BxBxiB', data)
 
-	if not checksum==_calculate_checksum(data[:-2]):
-		# Checksum error
-		...
+    if not checksum==_calculate_checksum(data[:-2]):
+        # Checksum error
+        ...
 
-	return address, status, value
+    return address, status, value
